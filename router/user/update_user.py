240b818cc6434b572e8afa74ac36dd9e8604f4c0
IamdebashisD <adebashisdas626@gmail.com>
2025-03-14 01:56:55 +0530

    fix some issue

--- router/user/update_user.py
+++ router/user/update_user.py
@@ -8,17 +8,17 @@ from sqlalchemy.exc import SQLAlchemyError
 update_user_bp = Blueprint('/update_user_bp', __name__)
 @update_user_bp.route('/update_user/<int:id>', methods=['PUT'])
 def update_user(id):
-    try:
-        user = session.query(User).filter_by(id=id).first()
-        if not user:
-            return jsonify({"error": "User not found"}), 404
-        
-        data = user_schema.load(request.json, partial=True)
-        user.name = data.get('name', user.name)
-        user.email = data.get('email', user.email)
-
+    try:    
         with session.begin():
+            user = session.query(User).filter_by(id=id).first()
+            if not user:
+                return jsonify({"error": "User not found"}), 404
+
+            data = user_schema.load(request.json, partial=True)
+            user.name = data.get('name', user.name)
+            user.email = data.get('email', user.email)
             session.commit()
+
         return jsonify({"Message": "User updated successfully!", "user": user_schema.dump(user)}), 200
     
     except ValidationError as err:
